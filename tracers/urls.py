a3bbf722f6779ff1583cfa6ef8152d9b432f9110
thovy <98632452+thovy@users.noreply.github.com>
2023-09-15 12:55:10 +0900

    feat: django project init
    tracers 프로젝트라는 이름의 프로젝트 초기화.

--- tracers/urls.py
+++ tracers/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Chaser project.
+URL configuration for tracers project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
