b505bd2f63e684f4654f0a74d79f5d2ecaa6f904
thovy <98632452+thovy@users.noreply.github.com>
2023-09-19 17:17:17 +0900

    test: image_model_test

--- detect/views.py
+++ detect/views.py
@@ -4,6 +4,12 @@ from django.core.files.storage import FileSystemStorage
 import uuid
 import os
 from . import yolov5_detect
+from .models import Image_contents
+import base64
+from django.http import HttpResponseRedirect, JsonResponse
+import json
+from django.shortcuts import redirect
+from .forms import ImageContentsForm
 
 # 이미지 파일 업로드 페이지
 def index(request):
@@ -36,3 +42,24 @@ def detect(request):
 
     # 디텍팅된 이미지를 보여주는 페이지로 이동
     return render(request, 'detect/result.html', {'image': result_img_path})
+
+
+# 업로드했던 이미지 목록 리스트(자신이 올린 사진목록만 or 관리자라면 모든 사진목록)
+def image_list(request):
+    if request.user.is_superuser:
+        # 관리자라면 모든 사진목록
+        target_images_contents_list = Image_contents.objects.all()
+    else:
+        # 자신이 올린 사진목록만
+        target_images_contents_list = Image_contents.objects.filter(user = request.user)
+    return render(request, 'detect/image_list.html', {'images_contents_list': target_images_contents_list})
+
+# 
+def image_detail(request, image_id):
+    image = Image_contents.objects.get(pk=image_id)
+    return render(request, 'detect/image_detail.html', {'image': image})
+
+# 촬영해서 업로드하는 페이지
+
+
+# video streaming 중 실시간 detecting
--- tracers/urls.py
+++ tracers/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from detect import views
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="index"),
-    path('detect/', views.detect, name="detect"),
+    path('image/',include('detect.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
