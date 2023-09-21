8194ae812b428b9ce63f4c9ac7bce40c348fb515
Aphian <rsw7876@naver.com>
2023-09-21 23:47:48 +0900

    signup_valid_customize

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
+# 연락처 입력에 대한 유효성 검사
 def validate_phone(phone):
     if phone[:4] != '010-':
         raise ValidationError(" '010-' 로 시작해주세요.")
@@ -14,5 +15,5 @@ class User(AbstractUser):
 
     # User name
     name = models.CharField(max_length=10)
-    # User phone
+    # User phone / 유효성 검사 함수 불러오기
     phone = models.CharField(max_length=20, validators=[validate_phone])
