4bcee0985d01378035cd07faaf5a758d717267fc
Dev-nenda <jooeon1104@gmail.com>
2023-09-30 23:48:36 +0900

    edit visualization.view.py & create select_date.html

--- visualization/urls.py
+++ visualization/urls.py
@@ -6,4 +6,5 @@ app_name = 'visualization'
 urlpatterns = [
     # /visualization/
     path('', views.visualize_data, name='visualize_data'),
+    path('visualize_data_by_date/', views.visualize_data_by_date, name='visualize_data_by_date'),
 ]
